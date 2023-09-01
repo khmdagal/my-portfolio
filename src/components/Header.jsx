d755471f89520139b56c1a1f72c8cf5c1ed5c1ac
khmdagal <khmdagal@gmail.com>
2023-09-01 18:39:31 +0100

    Improve Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import profileImage from "../Images/Profilet1.jpg";
 import "../CSS-Files/Header.css";
+
+
+
 
 function Header() {
   return (
-    <header>
-      <h1>Khdar Mohamed Dagal Portfolio</h1>
-    </header>
+   
+      <header>
+        <div className="image-container">
+          <img src={profileImage} alt="Profile_Picture" />
+        </div>
+      <h1 id="name">Khdar Mohamed Dagal</h1>
+      <h2 id="profession">Junior Web Developer</h2>
+      </header>
+ 
   );
 }
 
