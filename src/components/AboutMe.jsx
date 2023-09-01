a1eac651a71061416075240cb0bdd240b1b4cd24
khmdagal <khmdagal@gmail.com>
2023-09-01 18:41:16 +0100

    Add 3 Paragraph of introduction

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import "../CSS-Files/AboutMe.css";
+
+function AboutMe() {
+  return (
+    <div className="hero-main-container">
+      <div className="hero-intro-containter">
+        <p>
+          I am a junior full stack software developer from. I am
+          passionate about web development technologies.
+        </p>
+        <p>
+         I have designed, developed and deployed several projects ,and my mission is
+          to embrace a journey of continuous learning, crafting innovative web
+          applications.
+        </p>
+        <p>
+          This mission follows my successful completion of a rigorous 9-month software development
+          program at CodeYourFuture boot camp in Manchester.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default AboutMe;
