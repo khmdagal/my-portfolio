2f0f5d75538d52a7d802767f1f73c7fc0409070c
khmdagal <khmdagal@gmail.com>
2023-09-02 01:44:33 +0100

    Edit AboutMe and add mobile and tablet css rules

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,25 +1,28 @@
 import React from "react";
-
 import "../CSS-Files/AboutMe.css";
+
 
 function AboutMe() {
   return (
-    <div className="hero-main-container">
-      <div className="hero-intro-containter">
+    <div className="main-container">
+      <h1 className="title">About Me</h1>
+      <div className="intro-container">
         <p>
-          I am a junior full stack software developer from. I am
-          passionate about web development technologies.
+          I am a junior full stack software developer. I am passionate about web
+          development technologies.
         </p>
         <p>
-         I have designed, developed and deployed several projects ,and my mission is
-          to embrace a journey of continuous learning, crafting innovative web
-          applications.
+          I have designed, developed and deployed several projects ,and my
+          mission is to embrace a journey of continuous learning, crafting
+          innovative web applications.
         </p>
         <p>
-          This mission follows my successful completion of a rigorous 9-month software development
-          program at CodeYourFuture boot camp in Manchester.
+          This mission follows my successful completion of a rigorous 9-month
+          software development program at CodeYourFuture boot camp in
+          Manchester.
         </p>
       </div>
+
     </div>
   );
 }
