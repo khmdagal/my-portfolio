fccc8e1b4a4179c58670df24c4a4ee2c00f8320e
khmdagal <khmdagal@gmail.com>
2023-09-02 01:48:57 +0100

    Change App Component structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from "react";
-//import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import HomePage from "./components/HomePage";
+import AboutMe from "./components/AboutMe";
+import Skills from "./components/Skills";
 
 function App() {
+  
   return (
-    <div>
-      <Header />
-      <About />
-      <HomePage />
-      <Projects />
-      <Contact />
+    <div >
+        <Header />
+        <AboutMe />
+        <Skills />
+        <Projects />
+        <Contact />
     </div>
-  )
+  );
 }
 
 export default App;
