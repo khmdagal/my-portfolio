53a9cb98ca84897917481697fb701b0e1767dd98
khmdagal <khmdagal@gmail.com>
2023-09-02 17:01:14 +0100

    Restructure project component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,8 +3,6 @@ import "../CSS-Files/Projects.css";
 import Data from "../Data/JasonData.json";
 
 function Projects() {
-  
-
   return (
     <div className="mainContainer">
       <h2 className="title">Projects</h2>
@@ -12,15 +10,22 @@ function Projects() {
         {Data.Projects.map((project) => {
           return (
             <div className="projectCard" key={project.Project_Name}>
-              <h3 className="projectTitle">{project.Project_Name}</h3>
-              <span className="myRole">My Role: {project.My_role}</span>
-              <a className="demoLink" href={`${project.Demo}`}>
-                Project Demo
-              </a>
-              <p className="projectObjective">Objective: {project.Objective}</p>
-              <p className="techStuck">
-                Project Tech Stuck: {project.Tech_stuck}
-              </p>
+              <div className="project-intro">
+                <h3 className="projectTitle">{project.Project_Name}</h3>
+                <span className="myRole">My Role: {project.My_role}</span>
+                <a className="demoLink" href={`${project.Demo}`}>
+                  Demo
+                </a>
+              </div>
+
+              <div className="projectObjective">
+                <span>Objective </span>
+                <p>{project.Objective}</p>
+              </div>
+              <div className="techStuck">
+                <span>Project Tech Stuck </span>
+                <p>{project.Tech_stuck}</p>
+              </div>
             </div>
           );
         })}
