cf6571f4bc34843f17cbdab86407ed1df8a35ef2
khmdagal <khmdagal@gmail.com>
2023-09-02 15:06:35 +0100

    Add data follow from json file and improve styleSheet

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,57 +1,29 @@
 import React from "react";
 import "../CSS-Files/Projects.css";
+import Data from "../Data/JasonData.json";
 
 function Projects() {
+  console.log(Data.Projects.map((project) => project));
+
   return (
-    <div>
-      <h2>Projects</h2>
-      <div className="project">
-        <h3>CYF Group Buddy Project</h3>
-        <h4>Role: Developer</h4>
-        <h4>Demo</h4>
-        <p>
-          Objective: To cultivate collaborative teamwork, problem-solving skills
-          and a strategic programming mindset. Initiated dynamic brainstorming
-          sessions to conceive and develop a project with a significant positive
-          impact. Project Focus: The endeavor was centered on the utilization of
-          modern front-end technologies, particularly HTML, CSS and pure
-          JavaScript. Rather than relying on traditional frameworks, we
-          harnessed the capabilities of the following technologies.
-          Technologies: HTML, CSS and pure JavaScript.
-        </p>
-      </div>
-      <div className="project">
-        <h3>Spelling Test Practice Project</h3>
-        <h4>Role: Developer</h4>
-        <h4>Demo</h4>
-        <p>
-          Objective: The project was to undertaken overarching goal of creating
-          a modern web application to aid children preparation for statutory
-          spelling assessments. Front-end: Leveraged the powerful modern
-          technologies such as React.js, JavaScript and CSS to deliver
-          interactive user experience. Back-end: Employed Node.js and Express
-          library to stablish a seamless and efficient connection between front-
-          end and back-end components. Additionally, Incorporated PostgreSQL
-          database to management to robust data storage and retrieval. Core
-          Technologies: HTML, CSS, React.js JavaScript, Node.js, PostgreSQL and
-          Cypress
-        </p>
-      </div>
-      <div className="project">
-        <h3>The Collective Foundation Project</h3>
-        <h4>Role: Developer</h4>
-        <h4>Demo</h4>
-        <p>
-          Objective: This was CYF boot camp, and the initiative was to
-          undertaken to construct a fully operational CMS website catering to
-          the needs of the Collective Foundation Charity, based in Hungry.
-          Front-end: Blend React.js, JavaScript, CSS and Ant Design to shape a
-          dynamic visually engaging front-end. Back-end: Utilized Node.js to
-          ensure the smooth flow of data processing, impeccable communication
-          between client side and server side and integration with a PostgreSQL
-          database. Technologies: React.js, Ant Design, JavaScript, Node.js,
-          PostgreSQL, HTML5 and CSS3, deployed on render.com
-        </p>
+    <div className="mainContainer">
+      <h2 className="title">Projects</h2>
+      <div className="projectsContainer">
+        {Data.Projects.map((project) => {
+          return (
+            <div className="projectCard" key={project.Project_Name}>
+              <h3 className="projectTitle">{project.Project_Name}</h3>
+              <span className="myRole">My Role: {project.My_role}</span>
+              <a className="demoLink" href={`${project.Demo}`}>
+                Project Demo
+              </a>
+              <p className="projectObjective">Objective: {project.Objective}</p>
+              <p className="techStuck">
+                Project Tech Stuck: {project.Tech_stuck}
+              </p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
