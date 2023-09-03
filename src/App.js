7e7fd1dcc55d91c7f00cace94c804b404e824d49
khmdagal <khmdagal@gmail.com>
2023-09-03 20:38:17 +0100

    Add switch statement

--- src/App.js
+++ src/App.js
@@ -4,18 +4,38 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import AboutMe from "./components/AboutMe";
 import Skills from "./components/Skills";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 function App() {
-// do fetching here and pass props to the relevant component.
-  
+  // do fetching here and pass props to the relevant component.
+  let Component;
+  switch (window.location.pathname) {
+    case "/":
+      Component = <Header />;
+      break;
+    case "/aboutMe":
+      Component = <AboutMe />;
+      break;
+    case "/skills":
+      Component = <Skills />;
+      break;
+    case "/projects":
+      Component = <Projects />;
+      break;
+    case "/contact":
+      Component = <Contact />;
+      break;
+    default:
+      Component = null;
+      break;
+  }
   return (
-    <div >
-        <Header />
-        <AboutMe />
-        <Skills />
-        <Projects />
-      <Contact />
+    <div>
+      <Navbar />
+    
+      {Component}
+      
       <Footer />
     </div>
   );
