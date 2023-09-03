877f11ac548798fd1dc5a6f93cfe2b59f87b5eef
khmdagal <khmdagal@gmail.com>
2023-09-03 18:44:51 +0100

    Improve project demo

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,12 +8,20 @@ function Projects() {
       <h2 className="title">Projects</h2>
       <div className="projectsContainer">
         {Data.Projects.map((project) => {
+          console.log(typeof project.Demo);
           return (
             <div className="projectCard" key={project.Project_Name}>
               <div className="project-intro">
                 <h3 className="projectTitle">{project.Project_Name}</h3>
                 <span className="myRole">My Role: {project.My_role}</span>
-                <a className="demoLink" href={`${project.Demo}`}>
+                <a
+                  className="demoLink"
+                  href={`${
+                    typeof project.Demo === "string"
+                      ? project.Demo
+                      : project.Demo[0]
+                  }`}
+                >
                   Demo
                 </a>
               </div>
