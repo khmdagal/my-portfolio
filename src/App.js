c0beea9cec654188598b4c11f881504db0a17c57
Khadar Dagal <81495872+khmdagal@users.noreply.github.com>
2025-05-10 22:29:26 +0100

    Revert "Navbar rounting"
    This reverts commit 40f0ce63dcd983772f4db5ef350851d9ae19190f.

--- src/App.js
+++ src/App.js
@@ -9,56 +9,24 @@ import Navbar from "./components/Navbar";
 
 function App() {
   // do fetching here and pass props to the relevant component.
- let component
- switch (window.location.pathname) {
-      case "/home":
-        component = <Header />;
-        break;
-      case "/about":
-        component = <AboutMe />;
-        break;
-      case "/skills":
-        component = <Skills />;
-        break;
-      case "/projects":
-        component = <Projects />;
-        break;
-      case "/contact":
-        component = <Contact />;
-        break;
-      default:
-        component = <Header />;
-    }
-  // const handleScroll = (event) => {
-  //   const targetId = event.target.getAttribute("href");
-  //   const targetElement = document.querySelector(targetId);
-  //   if (targetElement) {
-  //     targetElement.scrollIntoView({ behavior: "smooth" });
-  //   }
-  // };
-  // const handleLinkClick = (event) => {
-  //   event.preventDefault();
-  //   const targetId = event.target.getAttribute("href");
-  //   const targetElement = document.querySelector(targetId);
-  //   if (targetElement) {
-  //     targetElement.scrollIntoView({ behavior: "smooth" });
-  //   }
-  // };
-  // const handleLinkClick = (event) => {
-  //   event.preventDefault();
-  //   const targetId = event.target.getAttribute("href");
-  //   const targetElement = document.querySelector(targetId);
-  //   if (targetElement) {
-  //     targetElement.scrollIntoView({ behavior: "smooth" });
-  //   }
-  // };                   
-  
   return (
     <div>
       <Navbar />
-      <div className="container">
-        {component}
-        </div>
+      <div id="home">
+        <Header />
+      </div>
+      <div id="about">
+        <AboutMe />
+      </div>
+      <div id="skills">
+        <Skills />
+      </div>
+      <div id="projects">
+        <Projects />
+      </div>
+      <div id="contact">
+        <Contact />
+      </div>
       <Footer />
     </div>
   );
