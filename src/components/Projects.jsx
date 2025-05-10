b8e139c367eda31384ba0decbb2c63636660e4ed
Khadar Dagal <81495872+khmdagal@users.noreply.github.com>
2025-05-10 18:10:51 +0100

    Add spinner while data is being loaded

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,11 @@
-import React,{ useEffect , useState} from "react";
+import React, { useEffect, useState } from "react";
+import { CircleLoader } from "react-spinners";
 import style from "../CSS-Files/Projects.module.css";
 import api from "../api";
 
 function Projects() {
   const [projectsData, setProjectsData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function getProjects() {
@@ -11,53 +13,74 @@ function Projects() {
       const projects = await response.json();
       setProjectsData(projects.result)
     }
-
     getProjects();
-  },[])
+  }, []);
+
+
+  useEffect(() => {
+    if (projectsData.length > 0) {
+      setLoading(false);
+    }
+  }, [projectsData]);
 
   return (
     <div className={style.mainContainer}>
-      <h2 className={style.title}>Projects</h2>
-      <div className={style.projectsContainer}>
-        {projectsData.map((project) => {
-          return (
-            <div className={style.projectCard} key={project.id}>
-              <div className={style.projectIntro}>
-                <h3 className={style.projectTitle}>{project.name}</h3>
-                <span className={style.myRole}>My Role: {project.role}</span>
-                <a
-                  className={style.demoLink}
-                  href={`${
-                    typeof project.demo === "string"
-                      ? project.demo
-                      : project.demo[0]
-                  }`}
-                >
-                 Project Demo
-                </a>
-              </div>
-
-              <div className={style.projectObjective}>
-                <span>Objective </span>
-                <p>{project.objective}</p>
-              </div>
-              
-              <div className={style.techStack}>
-                <span>Project Tech Stack </span>
-                <p>{project.tech_stack}</p>
-              </div>
-              <div className={style.frontend}>
-                <span>Front-end Tech Stack </span>
-                <p>{project.frontend}</p>
-              </div>
-              <div className={style.backend}>
-                <span>Back-end Tech Stack </span>
-                <p>{project.backend}</p>
-              </div>
+      {
+        loading ?
+          (
+            <div className={style.loaderContainer}>
+              <CircleLoader
+                height={200}
+                width={100}
+                size={50}
+                radius={50}
+              />
+              <p>... Projects data is being loaded 🤞</p>
             </div>
-          );
-        })}
-      </div>
+          ) :
+          <div>
+            <h2 className={style.title}>Projects</h2>
+            <div className={style.projectsContainer}>
+              {projectsData.map((project) => {
+                return (
+                  <div className={style.projectCard} key={project.id}>
+                    <div className={style.projectIntro}>
+                      <h3 className={style.projectTitle}>{project.name}</h3>
+                      <span className={style.myRole}>My Role: {project.role}</span>
+                      <a
+                        className={style.demoLink}
+                        href={`${typeof project.demo === "string"
+                            ? project.demo
+                            : project.demo[0]
+                          }`}
+                      >
+                        Project Demo
+                      </a>
+                    </div>
+
+                    <div className={style.projectObjective}>
+                      <span>Objective </span>
+                      <p>{project.objective}</p>
+                    </div>
+
+                    <div className={style.techStack}>
+                      <span>Project Tech Stack </span>
+                      <p>{project.tech_stack}</p>
+                    </div>
+                    <div className={style.frontend}>
+                      <span>Front-end Tech Stack </span>
+                      <p>{project.frontend}</p>
+                    </div>
+                    <div className={style.backend}>
+                      <span>Back-end Tech Stack </span>
+                      <p>{project.backend}</p>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+      }
     </div>
   );
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,10 +1,18 @@
 import React, { useState, useEffect } from "react";
+import { GridLoader } from "react-spinners";
 import api from "../api";
 import Card from "react-bootstrap/Card";
 import style from "../CSS-Files/Skills.module.css"
 
 export default function Skills() {
   const [skillsData, setSkillsData] = useState([])
+  const [loading, setLoading] = useState(true);
+  const color = "black";
+  const override = {
+    display: "block",
+    margin: "0 auto",
+    borderColor: "red",
+  };
 
   useEffect(() => {
     async function getSkills() {
@@ -16,20 +24,48 @@ export default function Skills() {
     getSkills();
   }, [])
 
-
+  useEffect(() => { 
+    if (skillsData.length > 0) {
+      setLoading(false);
+    }
+   }
+    , [skillsData]);
 
   return (
     <div className={style.mainContainer}>
-      <h2 className={style.title}>Skills</h2>
-      <div className={style.skillsContainer}>
-        {skillsData.map((skill) => {
-          return (
-            <Card className={style.card} key={skill.name}>
-              <h2 className={style.skillName}>{skill.name}</h2>
-            </Card>
-          );
-        })}
-      </div>
+      {
+        loading ? (
+          <div className={style.loaderContainer}>
+            <GridLoader
+              color={color}
+              loading={loading}
+              cssOverride={override}
+              height={100}
+              size={30}
+              aria-label="Loading Spinner"
+              data-testid="loader"
+            />
+            <p>...Skills data is being loaded 🤞</p>
+          </div>
+        ) :
+          <div>
+            <h1 className={style.title}>Skills</h1>
+            <div className={style.skillsContainer}>
+              {
+                skillsData.map((eachSkill) => {
+                  return ( 
+                    <Card key={eachSkill._id} className={style.card}>
+                      <Card.Img variant="top" src={eachSkill.image} />
+                      <Card.Body>
+                        <Card.Title>{eachSkill.name}</Card.Title>
+                      </Card.Body>
+                    </Card>
+                  )
+                })
+              }
+            </div>
+          </div>
+      }
     </div>
   );
 }
